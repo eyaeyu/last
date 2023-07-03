fe941d02e4ab7b74c9bf7f8a6e1790604c655897
eyaeyu <eyobchiksa26@gmail.COM>
2023-07-03 14:54:19 -0700

    first commit

---
+++ assi.java
@@ -0,0 +1,18 @@
+public class assi {
+    
+}
+class call{
+    public static void main(String[] args){
+        assi s= new assi()
+        s.eyob();
+        s.hareg();
+        s.hassen();
+        s.eyerus();
+        s.khader();
+        s.ferdu();
+        s.fasika();
+        s.fasil();
+                
+
+    }
+}
