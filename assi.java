85c9c8ba1e79371060a6ffdc5f96c8d46e174236
fasikakassahun <fasikakassahun1912@gmail.com>
2023-07-03 15:27:08 -0700

    fasikas method

--- assi.java
+++ assi.java
@@ -6,6 +6,13 @@ public class assi {
         system.out.println("debre brehan university");
 
     }
+void fasika(){
+        system.out.println("fasika kassahun");
+        system.out.println("4100/13");
+        system.out.println("software engineer");
+        system.out.println("and also web developer");
+
+    }
     
 }
 class call{
